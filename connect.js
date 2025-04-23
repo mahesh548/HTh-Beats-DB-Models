a09eae1ffc73ab85a289a70148cb383fb201e4b1
Mahesh <mkmjnp5@gmail.com>
2025-04-23 22:56:19 +0530

    Refactor database connection to use environment variables for production and local databases

--- connect.js
+++ connect.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
-
 const connectDB = async () => {
+  const prodDatabase = process.env.PROD_DATABASE;
+  const localDatabase = process.env.TEST_DATABASE;
   try {
-    await mongoose.connect("mongodb://localhost/cache");
-    console.log("Cache database connected..");
+    await mongoose.connect(prodDatabase);
+    console.log("Database connected..");
   } catch (error) {
     console.log(error.message);
   }
