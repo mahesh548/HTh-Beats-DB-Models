0243e120bd9983d7e52d1b943dc89664a554d6f9
Mahesh <mkmjnp5@gmail.com>
2025-04-26 10:26:56 +0530

    Refactor database connection logic to use a single connection string based on environment

--- connect.js
+++ connect.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
-  const prodDatabase = process.env.PROD_DATABASE;
-  const localDatabase = process.env.TEST_DATABASE;
+  const connectionString =
+    process.env.ENVIROMENT == "LOCAL"
+      ? process.env.LOCAL_DATABASE
+      : process.env.PROD_DATABASE;
   try {
-    await mongoose.connect(prodDatabase);
+    await mongoose.connect(connectionString);
     console.log("Database connected..");
   } catch (error) {
     console.log(error.message);
